fab2cb59af4116da648ade71bdff689a06b6a4bf
sh3244 <sh3244@nyu.edu>
2019-12-06 12:29:00 -0500

    use xrange python

--- PythonBenchmark.py
+++ PythonBenchmark.py
@@ -1,8 +1,8 @@
 # |||||||| Python version ||||||||
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-for i in range(100000000):
-    for j in range(len(array)):
+for i in xrange(100000000):
+    for j in xrange(len(array)):
         array[j] += 1
         
 print(array)
