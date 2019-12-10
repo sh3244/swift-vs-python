0e6ab9eb91a8c7d3d150d28f50ab6074bd906b8d
Utkarsh Gupta <ug267@nyu.edu>
2019-12-10 11:20:12 -0800

    Update C++Benchmark.cpp

--- C++Benchmark.cpp
+++ C++Benchmark.cpp
@@ -1,57 +1,26 @@
 #include<iostream>
-#include <vector>
 
-#define TIMES 10000000  //add num times you want to run
+#define TIMES 1000000  //add num times you want to run
 
 using namespace std;
 
-vector<int> v {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-vector<int>::iterator ptr;
+uint32_t arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-void calcuC(vector<int> &v, int length)
+
+void calcuC(uint32_t v,int length)
 {
-
-    for(uint8_t ctr = length; ctr != 0; --ctr)
-        for(uint32_t lpc = TIMES; lpc != 0; --lpc)
-            v[ctr-1] += 1;
-
-
-    // _asm
-    // {
-    //     mov edi,TIMES
-    //     start:
-    //     mov esi,0
-    //     mov ecx,lengthOfArray
-    //     label:
-    //     mov edx,x
-    //     push edx
-    //     mov eax,DWORD PTR [edx + esi*4]
-    //     mov edx,y
-    //     mov ebx,DWORD PTR [edx + esi*4]
-    //     add eax,ebx
-    //     pop edx
-    //     mov [edx + esi*4],eax
-    //     inc esi
-    //     loop label
-    //     dec edi
-    //     cmp edi,0
-    //     jnz start
-    // };
-
+    for(uint8_t ctr = length; ctr > 0; --ctr)
+        for(uint32_t lpc = 1000000; lpc != 0; --lpc)
+            v[ctr] += 1;
 }
 
+
 int main() {
-    setbuf(stdout, NULL);
-    calcuC(v, 10);
+    setbuf(stdout,NULL);
+    calcuC(v,10);
     
-    cout << "[";
-    for (const auto i: v) {
-        if (i == v[v.size() - 1]) {
-            cout << i << "]";
-        } else {
-            cout << i << ", ";
-        }
-    }
+    for (const auto i: v)
+      cout << i << ' ';
       
     return 0;
 }
